2b0185af90cbcec89db3f3d3e03888d80ef147bc
mikecancilla <baronvonmike@gmail.com>
2019-06-18 10:10:31 -0700

    Full camera movement!

--- GraphicsPad/Camera.cpp
+++ GraphicsPad/Camera.cpp
@@ -1,6 +1,8 @@
 #include "Camera.h"
 #include <gtx\transform.hpp>
 
+const float Camera::MOVEMENT_SPEED = 0.1f;
+
 Camera::Camera()
 	: viewDirection(0, 0, -1)
 	, UP(0, 1, 0)
@@ -26,10 +28,10 @@ void Camera::mouseUpdate(const glm::vec2 & newMousePosition)
 	// For a vertical camera rotation, you currently have a look direction and and up, these are 90 degrees from each other.
 	// Take the cross product of the view and up, to get a perpendicular vector to the right
 	// Rotate up or down around this perpendicular cross product result vector
-	glm::vec3 toRotateAround = glm::cross(viewDirection, UP);
+    strafeDirection = glm::cross(viewDirection, UP);
 
 	glm::mat4 rotator = glm::rotate(-mouseDelta.x / ROTATIONAL_SPEED, UP) *
-		                glm::rotate(-mouseDelta.y / ROTATIONAL_SPEED, toRotateAround); // Rotate in X and Y in one step now
+		                glm::rotate(-mouseDelta.y / ROTATIONAL_SPEED, strafeDirection); // Rotate in X and Y in one step now
 
     // Update the Y rotation
 	viewDirection = glm::mat3(rotator) * viewDirection;
@@ -46,4 +48,34 @@ void Camera::mouseUpdate(const glm::vec2 & newMousePosition)
 glm::mat4 Camera::getWorldToViewMatrix() const
 {
 	return glm::lookAt(position, position + viewDirection, UP);
+}
+
+void Camera::moveForward()
+{
+	position += MOVEMENT_SPEED * viewDirection;
+}
+
+void Camera::moveBackward()
+{
+	position += -MOVEMENT_SPEED * viewDirection;
+}
+
+void Camera::strafeLeft()
+{
+	position += -MOVEMENT_SPEED * strafeDirection;
+}
+
+void Camera::strafeRight()
+{
+	position += MOVEMENT_SPEED * strafeDirection;
+}
+
+void Camera::moveUp()
+{
+	position += MOVEMENT_SPEED * UP;
+}
+
+void Camera::moveDown()
+{
+	position += -MOVEMENT_SPEED * UP;
 }
--- GraphicsPad/Camera.h
+++ GraphicsPad/Camera.h
@@ -8,6 +8,9 @@ class Camera
 	glm::vec3 viewDirection; // Needs to be normalized (a unit vector)
 	const glm::vec3 UP;
 	glm::vec2 oldMousePosition;
+	glm::vec3 strafeDirection;
+
+	static const float MOVEMENT_SPEED;
 
 public:
 	Camera();
@@ -15,5 +18,12 @@ public:
 
 	void mouseUpdate(const glm::vec2 &newMousePosition);
 	glm::mat4 getWorldToViewMatrix() const;
+
+	void moveForward();
+	void moveBackward();
+	void strafeLeft();
+	void strafeRight();
+	void moveUp();
+	void moveDown();
 };
 
