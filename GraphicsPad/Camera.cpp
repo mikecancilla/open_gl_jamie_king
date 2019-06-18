2867c7957346b4b76f7fd66ae0fe8a0e15515d56
mikecancilla <baronvonmike@gmail.com>
2019-06-17 17:43:00 -0700

    Cleaned up X and Y rotation code to be a bit faster.

--- GraphicsPad/Camera.cpp
+++ GraphicsPad/Camera.cpp
@@ -23,16 +23,22 @@ void Camera::mouseUpdate(const glm::vec2 & newMousePosition)
 
 	const float ROTATIONAL_SPEED = 128.f;
 
-	// 1 degree per mouse pixel, update the X rotation
-	viewDirection = glm::mat3(glm::rotate(-mouseDelta.x / ROTATIONAL_SPEED, UP)) * viewDirection;
-
 	// For a vertical camera rotation, you currently have a look direction and and up, these are 90 degrees from each other.
 	// Take the cross product of the view and up, to get a perpendicular vector to the right
 	// Rotate up or down around this perpendicular cross product result vector
 	glm::vec3 toRotateAround = glm::cross(viewDirection, UP);
 
+	glm::mat4 rotator = glm::rotate(-mouseDelta.x / ROTATIONAL_SPEED, UP) *
+		                glm::rotate(-mouseDelta.y / ROTATIONAL_SPEED, toRotateAround); // Rotate in X and Y in one step now
+
+    // Update the Y rotation
+	viewDirection = glm::mat3(rotator) * viewDirection;
+
+	// 1 degree per mouse pixel, update the X rotation
+	//viewDirection = glm::mat3(glm::rotate(-mouseDelta.x / ROTATIONAL_SPEED, UP)) * viewDirection;
+
 	// Update the Y rotation
-	viewDirection = glm::mat3(glm::rotate(-mouseDelta.y / ROTATIONAL_SPEED, toRotateAround)) * viewDirection;
+	//viewDirection = glm::mat3(glm::rotate(-mouseDelta.y / ROTATIONAL_SPEED, toRotateAround)) * viewDirection;
 
 	oldMousePosition = newMousePosition;
 }
