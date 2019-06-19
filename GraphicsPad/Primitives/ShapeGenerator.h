bb2a8dde12d78f225c00546a7c2f66e6434514d3
mikecancilla <baronvonmike@gmail.com>
2019-06-18 17:33:29 -0700

    Added makeTeapot.  The lid does not draw for some reason.

--- GraphicsPad/Primitives/ShapeGenerator.h
+++ GraphicsPad/Primitives/ShapeGenerator.h
@@ -6,6 +6,31 @@ class ShapeGenerator
     static ShapeData makePlaneVerts(GLuint dimensions);
     static ShapeData makePlaneIndices(GLuint dimensions);
 
+	// Teapot helpers
+	static void generatePatches(float * v, float * n, float * tc, unsigned short* el, int grid);
+	static void moveLid(int grid, float *v, glm::mat4 lidTransform);
+	static void buildPatchReflect(int patchNum,
+                            		float *B, float *dB,
+                            		float *v, float *n,
+                            		float *tc, unsigned short *el,
+                            		int &index, int &elIndex, int &tcIndex, int grid,
+                            		bool reflectX, bool reflectY);
+	static void buildPatch(glm::vec3 patch[][4],
+                    		float *B, float *dB,
+                    		float *v, float *n, float *tc,
+                    		unsigned short *el,
+                    		int &index, int &elIndex, int &tcIndex,
+                    		int grid, glm::mat3 reflect,
+                    		bool invertNormal);
+	static void getPatch(int patchNum, glm::vec3 patch[][4], bool reverseV);
+	static void computeBasisFunctions(float * B, float * dB, int grid);
+	static glm::vec3 evaluate(int gridU, int gridV, float *B, glm::vec3 patch[][4]);
+	static glm::vec3 evaluateNormal(int gridU,
+                                    int gridV,
+		                            float *B,
+                                    float *dB,
+                                    glm::vec3 patch[][4]);
+
 public:
     ShapeGenerator();
     virtual ~ShapeGenerator();
@@ -14,5 +39,6 @@ public:
     static ShapeData makeCube();
     static ShapeData makeArrow();
     static ShapeData makePlane(GLuint dimensions = 10);
+    static ShapeData makeTeapot(GLuint tesselation = 10, const glm::mat4& lidTransform = glm::mat4());
 };
 
